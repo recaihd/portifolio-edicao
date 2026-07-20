b916ffe2c162f91664a800b4dfdcabe3f79f84ed
recaihd <ricardosousapixtudo@gmail.com>
2026-07-20 10:59:11 -0300

    audio

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-const MusicaSite = new Audio('audiozin-cortado.mp3');
-
+const MusicaSite = new Audio('./audiozin-cortado.mp3');
 MusicaSite.loop = true; 
 
-MusicaSite.play();   
+function ativarAudio() {
+    MusicaSite.play()
+        .then(() => {
+            console.log("Música iniciada com sucesso!");
+            document.removeEventListener('click', ativarAudio);
+            document.removeEventListener('touchstart', removeAtivarAudio);
+        })
+        .catch((error) => {
+            console.error("Erro ao tocar a música:", error);
+        });
+}
+
+function removeAtivarAudio() {
+    ativarAudio();
+}
+
+document.addEventListener('click', ativarAudio);
+document.addEventListener('touchstart', removeAtivarAudio);
